1826276e025160c7165355d7f3f5a87c538e1271
MenaMema <amenabarrera@gmail.com>
2022-07-05 18:03:07 +0200

    finished

---
+++ solution.js
@@ -0,0 +1,14 @@
+let introSentece = "hello, my name is Antonio and I am";
+let age = 29;
+console.log(introSentece);
+console.log(age);
+console.log();
+console.log(introSentece + " " + age);
+//The + not only combines both sentences but also convert into a string the age variable
+console.log("--------------------------");
+const integer = "1005";
+console.log(parseInt(integer));
+
+console.log("--------------------");
+const float = "10.05";
+console.log(parseFloat(float));
